888928c3d420f01a2d1434b1856c87172d3c8d32
shullmb <shullmb@gmail.com>
2018-06-12 16:18:54 -0700

    finishing node app setup

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ app.use(bp.urlencoded({extended:false}));
 
 // default route
 app.get('/', function(req,res) {
-    res.sendFile('html/index.html');
+    // TODO: add links to index.html so that users can navigate
+    res.sendFile(__dirname + '/static/html/index.html');
 })
+
+app.get('/skills', function(req,res) {
+    var skills = fs.readFileSync('./data.json');
+    skills = JSON.parse(skills);
+    // res.send(skills);
+    // TODO: render this array of skills into ejs template
+    res.render('skills', {skills: skills});
+})
+
+// TODO: Add GET route that returns static pate containing form
+
+// TODO: Add POST route that writes new skill to the file, redirects to skills index
+// TODO: Form should have action='/skills' and method='POST'
 
 app.listen(process.env.PORT || 3000);
